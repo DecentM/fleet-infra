d58e7691e4c688407e9de245fd9ea04b1b17c4f1
DecentM <decentm@decentm.com>
2024-08-20 01:32:33 +0300

    refactor: move join information to a temp join config

--- bin/join-host-as-node.sh
+++ bin/join-host-as-node.sh
@@ -16,4 +16,17 @@ read -r token
 printf "> Cert Hash: "
 read -r cert_hash
 
-kubeadm join "$master_ip:6443" --token "$token" --discovery-token-ca-cert-hash "$cert_hash" --config infrastructure/base/layer0/config/join-config.yaml
+cat <<EOF >/tmp/join-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: JoinConfiguration
+discovery:
+  bootstrapToken:
+    token: "$token"
+    apiServerEndpoint: "$master_ip:6443"
+    caCertHashes:
+      - "$cert_hash"
+EOF
+
+kubeadm join --config infrastructure/base/layer0/config/join-config.yaml --config /tmp/join-config.yaml
+
+rm -f /tmp/join-config.yaml
