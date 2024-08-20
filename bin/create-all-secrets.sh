e8f87c2fe4533885c1432bdc46c01c2753bcc568
DecentM <decentm@decentm.com>
2024-08-20 22:16:44 +0300

    chore: add radarr postgres credentials

--- bin/create-all-secrets.sh
+++ bin/create-all-secrets.sh
@@ -73,3 +73,9 @@ create_secret \
     prowlarr-postgres-credentials \
     "POSTGRES_USER POSTGRES_PASSWORD POSTGRES_DB" \
     "apps/base/servarr/secrets/sealed-prowlarr-postgres-credentials.yaml"
+
+create_secret \
+    app-servarr \
+    radarr-postgres-credentials \
+    "POSTGRES_USER POSTGRES_PASSWORD POSTGRES_DB" \
+    "apps/base/servarr/secrets/sealed-radarr-postgres-credentials.yaml"
