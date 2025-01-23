c6f47f3b1b6a8af0df5497e81e726200a8c60d14
DecentM <decentm@decentm.com>
2025-01-23 23:25:05 +0000

    feat: run Concourse

--- bin/create-all-secrets.sh
+++ bin/create-all-secrets.sh
@@ -97,19 +97,19 @@ create_secret \
     "AWS_ACCESS_KEY_ID AWS_ENDPOINTS AWS_SECRET_ACCESS_KEY" \
     "infrastructure/production/layer1/sealed-s3-secret.yaml"
 
-# create_secret \
-#     app-concourse \
-#     local-users \
-#     generic \
-#     "value main-team-members" \
-#     "apps/base/concourse/secrets/sealed-local-users.yaml"
+create_secret \
+    app-concourse \
+    local-users \
+    generic \
+    "value main-team-members" \
+    "apps/base/concourse/secrets/sealed-local-users.yaml"
 
-# create_secret \
-#     concourse-main \
-#     npm \
-#     generic \
-#     "automation-token" \
-#     "apps/base/concourse/secrets/sealed-npm.yaml"
+create_secret \
+    concourse-main \
+    npm \
+    generic \
+    "automation-token" \
+    "apps/base/concourse/secrets/sealed-npm.yaml"
 
 create_secret \
     app-photoprism \
