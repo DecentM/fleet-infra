300d22b8d6290c529893bc8b0159f529a3d2b01c
DecentM <decentm@decentm.com>
2026-03-02 03:33:05 +0200

    chore: take out yarn and most deps

--- .concourse-tsrc.ts
+++
@@ -1,9 +0,0 @@
-import { rc } from "@decentm/concourse-ts-cli";
-
-export default rc({
-    compile: {
-        input: '.ci/src/*.pipeline.ts',
-        output: '.ci/dist',
-        project: './tsconfig.json'
-    }
-})
